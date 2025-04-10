af3c8514e0996037654b1538d79b3af752d77440
Ravi-Gupta777 <ravi@cloudtechservice.com>
2025-04-10 14:16:23 +0545

    minor fixes

--- eks.tf
+++ eks.tf
@@ -17,6 +17,7 @@ module "eks" {
   eks_managed_node_group_defaults = {
     instance_types = ["t3.small"]
   }
+
   eks_managed_node_groups = {
     main = {
       ami_type       = "AL2023_x86_64_STANDARD"
