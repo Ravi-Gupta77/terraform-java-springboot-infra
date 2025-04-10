ff4640997a7f0c6504f287dc5e304cc4375fde1b
Ravi-Gupta777 <ravi@cloudtechservice.com>
2025-04-10 10:50:19 +0545

    minor fixes

--- eks.tf
+++ eks.tf
@@ -9,14 +9,18 @@ module "eks" {
   cluster_name                   = "java-springboot-app"
   cluster_version                = "1.28"
   vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  subnet_ids                     = module.vpc.public_subnets
   cluster_endpoint_public_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
     main = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.small"]
       min_size       = 1
       max_size       = 2
       desired_size   = 2
-      instance_types = ["t3.small"]
     }
   }
 
